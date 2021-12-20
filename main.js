7103c5e2a56755eb9ed9db8845d1b337a0cba7ae
jishnu06 <84184331+jishnu06@users.noreply.github.com>
2021-12-20 15:37:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(550, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses)
+}
+function draw(){
+    background("#33aaff");
+}
+function modelLoaded(){
+    console.log("Posenet is initialized!");
+}
+function gotPoses(results){
+    if (results.length > 0){
+        console.log(results);
+    }
+}
